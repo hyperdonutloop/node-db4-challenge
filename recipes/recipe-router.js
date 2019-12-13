6149558a0dff322bcdeb2afe4d069784bbde96f4
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-12 21:56:18 -0800

    built out get request to get all recipes. tested with insomnia and GET request is successful

--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -1,5 +1,15 @@
 const express = require('express');
-
+const Recipes = require('./recipe-model.js');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  Recipes.getRecipes()
+    .then(recipes => {
+      res.status(200).json(recipes)
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: "Failed to retrieve recipes", error })
+    })
+})
 
 module.exports = router;
